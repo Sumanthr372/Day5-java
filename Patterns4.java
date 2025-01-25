1b6333d9f9dff8915ea9c4987a39f4b4e72e1e32
Sumanthr372 <151545106+Sumanthr372@users.noreply.github.com>
2025-01-25 12:01:27 +0530

    v1

--- Patterns4.java
+++ Patterns4.java
@@ -2,20 +2,18 @@
 public class Patterns4 {
 
 	public static void main(String[] args) {
-		
-		for (int r = 1; r <= 30; r++) {
-			for(int k=1;k<=20;k++) {
+
+		for (int r = 1; r <= 10; r++) {
+			for (int k = 1; k <= 20; k++) {
 				System.out.print(" ");
 			}
 			for (int c = 1; c <= 15; c++) {
-				if ((r == 1) || (r == 30)) {
+				if ((r == 1) || (r == 10)) {
 					System.out.print("*");
-				}
-				else {
+				} else {
 					if ((c == 1) || (c == 15)) {
 						System.out.print("*");
-					}
-					else {
+					} else {
 						System.out.print(" ");
 					}
 				}
